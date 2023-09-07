dd11eed3f51d3faadff0aad5fcfec23f6e5e9dea
juli81n7 <112468707+juli81n7@users.noreply.github.com>
2023-09-07 10:48:49 +0200

    Lavet

--- script.js
+++ script.js
@@ -1,20 +1,57 @@
 const url = "https://kea-alt-del.dk/kata-distortion/";
 const output = document.querySelector("h1");
+const newUpdate = document.querySelector("p");
+let previousNumber = 0;
+const bar = document.querySelector(".innerbar");
 
 newData();
 
 async function newData() {
-  const respons = await fetch(url, {
+  const response = await fetch(url, {
     method: "GET",
   });
-  const data = await respons.json();
+  const data = await response.json();
   console.log(data);
 
   display(data);
 }
 
-setInterval(newData, 5000);
+setInterval(newData, 10000);
 
-function display(data) {
-  output.textContent = "Du er nummer " + data.inQueue + " i køen";
+function display(newNumber) {
+  const difference = newNumber.inQueue - previousNumber;
+  console.log("nu1");
+  newUpdate.textContent = "Opdateret " + newNumber.loggedAt;
+  if (difference > 0) {
+    console.log("nu2");
+    let i = previousNumber;
+    const interval = setInterval(() => {
+      console.log("nu3");
+      output.textContent = "Du er nummer " + i + " i køen";
+
+      i++;
+      if (i > newNumber.inQueue) {
+        clearInterval(interval);
+      }
+    }, 50);
+  } else if (difference < 0) {
+    console.log("nu4");
+    let i = previousNumber;
+    const interval = setInterval(() => {
+      console.log("nu5");
+      output.textContent = "Du er nummer " + i + " i køen";
+      i--;
+      if (i < newNumber.inQueue) {
+        clearInterval(interval);
+      }
+    }, 10);
+  } else {
+    // The numbers are the same, no need to count.
+    console.log(newNumber.inQueue);
+  }
+  let newNumPercent = 100 - (newNumber.inQueue / 25) * 100;
+
+  bar.style.setProperty("--newWidth", newNumPercent + "%");
+  // Update the previous number.
+  previousNumber = newNumber.inQueue;
 }
